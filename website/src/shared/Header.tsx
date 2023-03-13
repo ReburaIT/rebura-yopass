eac9cb2272359f5a055202438e9fa42f65ae00b6
Hitesh.Patel <hitesh.patel@rebura.com>
2023-03-13 13:36:25 +0000

    updated header

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   // const location = useLocation();
   // const isOnUploadPage = location.pathname.includes('upload');
   const base = process.env.PUBLIC_URL || '';
-  const home = base + '/#/';
+  const home = base; //+ '/#/';
   // const upload = base + '/#/upload';
   return (
     <AppBar position="static" color="transparent" sx={{ marginBottom: 4 }}>
